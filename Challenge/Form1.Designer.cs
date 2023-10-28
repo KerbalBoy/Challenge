29c873c0c4f1d24abf50b1add3f869449eb15315
KerbalBoy <miles.sluyter@gmail.com>
2023-10-28 12:03:25 +0100

    TEsting here

--- Challenge/Form1.Designer.cs
+++ Challenge/Form1.Designer.cs
@@ -28,13 +28,62 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.clickMe = new System.Windows.Forms.Button();
+            this.textHere = new System.Windows.Forms.Label();
+            this.projectName = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // clickMe
+            // 
+            this.clickMe.Location = new System.Drawing.Point(340, 217);
+            this.clickMe.Name = "clickMe";
+            this.clickMe.Size = new System.Drawing.Size(75, 23);
+            this.clickMe.TabIndex = 0;
+            this.clickMe.Text = "Click Me!";
+            this.clickMe.UseVisualStyleBackColor = true;
+            this.clickMe.Click += new System.EventHandler(this.clickMe_Click);
+            // 
+            // textHere
+            // 
+            this.textHere.AutoSize = true;
+            this.textHere.Location = new System.Drawing.Point(456, 217);
+            this.textHere.Name = "textHere";
+            this.textHere.Size = new System.Drawing.Size(81, 13);
+            this.textHere.TabIndex = 1;
+            this.textHere.Text = "Text goes here!";
+            this.textHere.Click += new System.EventHandler(this.textHere_Click);
+            // 
+            // projectName
+            // 
+            this.projectName.AutoSize = true;
+            this.projectName.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.projectName.Location = new System.Drawing.Point(225, 34);
+            this.projectName.Name = "projectName";
+            this.projectName.Size = new System.Drawing.Size(502, 39);
+            this.projectName.TabIndex = 3;
+            this.projectName.Text = "PROJECT NAME GOES HERE";
+            this.projectName.Click += new System.EventHandler(this.textHere_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.projectName);
+            this.Controls.Add(this.textHere);
+            this.Controls.Add(this.clickMe);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button clickMe;
+        private System.Windows.Forms.Label textHere;
+        private System.Windows.Forms.Label projectName;
     }
 }
 
--- Challenge/Form1.cs
+++ Challenge/Form1.cs
@@ -16,5 +16,15 @@ namespace Challenge
         {
             InitializeComponent();
         }
+
+        private void textHere_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void clickMe_Click(object sender, EventArgs e)
+        {
+            textHere.Text = "I have been clicked!";
+        }
     }
 }
